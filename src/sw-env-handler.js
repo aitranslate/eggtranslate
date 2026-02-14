0a986c38b4cd0b45050a744ead688d2c3be3108a
eclipse005 <277141767@qq.com>
2026-02-14 19:54:35 +0800

    refactor: 移除Service Worker相关代码
    删除Service Worker及其注册逻辑，包括sw.js、sw-register.js和sw-env-handler.js文件

--- src/sw-env-handler.js
+++
@@ -1,8 +0,0 @@
-// 只在生产环境注册 Service Worker
-if (import.meta.env.PROD) {
-  // 动态加载 sw-register.js 脚本
-  const script = document.createElement('script');
-  script.src = '/sw-register.js';
-  script.async = true;
-  document.head.appendChild(script);
-}
